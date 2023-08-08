2166d20d48c25a6e8ff03592f01920686ac2d772
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-08-08 09:43:04 -0300

    Classe Descanso

---
+++ lab02/Controle_Institucional_coiSA/Coisa.java
@@ -0,0 +1,8 @@
+package Controle_Institucional_coiSA;
+
+public class Coisa{
+    public static void main(String[] args){
+        Descanso descanso = new Descanso(30);
+        System.out.println(descanso);
+    }
+}
