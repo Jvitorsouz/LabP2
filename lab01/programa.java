78f08a060bcbd5f3594e1c61820480fd79f8b98e
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-08-08 08:33:58 -0300

    versão final

---
+++ lab01/programa.java
@@ -0,0 +1,11 @@
+/**
+* Laboratório de Programação 2 - Lab 1
+*
+* @author João Vitor da Silva Souza - 122210993
+*/
+
+public class programa{
+	public static void main(String[] args){
+		System.out.println("Hello Dirlididi!");
+	}
+}
