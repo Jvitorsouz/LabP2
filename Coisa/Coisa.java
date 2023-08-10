5c723deb678dd787ac9cb1d423dfaa1329ab0d32
João Vitor da Silva Souza <129335518+Jvitorsouz@users.noreply.github.com>
2023-08-10 11:52:24 -0300

    Delete Coisa directory

--- Coisa/Coisa.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Coisa{
-    public static void main(String[] args){
-        Descanso descanso = new Descanso();
-        System.out.println(descanso.getStatusGeral());
-        
-    }
-}
