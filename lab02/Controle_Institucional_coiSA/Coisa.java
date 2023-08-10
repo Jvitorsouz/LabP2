12c46cd922e00d3ab0149141c6973c68a5c9f4ad
João Vitor da Silva Souza <129335518+Jvitorsouz@users.noreply.github.com>
2023-08-10 11:08:19 -0300

    Delete lab02/Controle_Institucional_coiSA directory

--- lab02/Controle_Institucional_coiSA/Coisa.java
+++
@@ -1,8 +0,0 @@
-package Controle_Institucional_coiSA;
-
-public class Coisa{
-    public static void main(String[] args){
-        Descanso descanso = new Descanso(30);
-        System.out.println(descanso);
-    }
-}
