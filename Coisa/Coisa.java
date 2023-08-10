7a5d6907461c7a575972457859c3647d9bd79f6c
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-08-10 11:14:58 -0300

    Programa

---
+++ Coisa/Coisa.java
@@ -0,0 +1,9 @@
+
+
+public class Coisa{
+    public static void main(String[] args){
+        Descanso descanso = new Descanso();
+        System.out.println(descanso.getStatusGeral());
+        
+    }
+}
