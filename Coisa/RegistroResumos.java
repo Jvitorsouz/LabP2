57a5417a970082e57f79c568698057b4038226c5
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-08-15 20:09:38 -0300

    Classe Atualizada

--- Coisa/RegistroResumos.java
+++ Coisa/RegistroResumos.java
@@ -2,16 +2,19 @@ public class RegistroResumos{
 
     private int numeroDeResumos;
     private String[] resumos;
+    private String[] temas;
 
     public RegistroResumos(int numeroDeResumos){
         this.numeroDeResumos = numeroDeResumos;
         resumos = new String[numeroDeResumos];
+        temas = new String[numeroDeResumos];
     }
 
     
     private int aux = 0;
 
     public void adiciona(String tema, String resumo){
+        temas[aux] = tema;
         String texto = tema + ": " + resumo;
         resumos[aux] = texto;
         aux++;
@@ -27,6 +30,16 @@ public class RegistroResumos{
 
     public String imprimeResumos(){
         return "- " + aux + " resumo(s) cadastrado(s)";
+    }
+
+    public boolean temResumo(String tema){
+        boolean verifica = false;
+        for(int i=0; i<aux; i++){
+            if(temas[i].equals(tema)){
+                verifica = true;
+            }
+        }
+        return verifica;
     }
 
 }
