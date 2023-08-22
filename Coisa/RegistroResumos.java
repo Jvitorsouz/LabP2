d8475f9ddc7eadcc8102bca5b79cac07081c02a7
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-08-22 09:51:46 -0300

    classe atualizada

--- Coisa/RegistroResumos.java
+++ Coisa/RegistroResumos.java
@@ -1,17 +1,58 @@
+/**
+* Classe responsavel por registrar os resumos dos estudados realizados durante o período.
+* Classe usada no Controle Institucional da Situação Acadêmica.
+* 
+* @author João Vitor da Silva Souza
+*/
 public class RegistroResumos{
 
+    /**
+    * Quantidade limite de resumos no formato inteiro. 
+    * Responsavel por definir a quantidade limite de resumos que podem ser registrados.
+    *
+    */
     private int numeroDeResumos;
+
+    /**
+    * Uma lista no formato String. 
+    * Responsavel pelo armazenamento dos resumos.
+    *
+    */
     private String[] resumos;
+
+    /**
+    * Uma lista no formato String. 
+    * Responsavel pelo armazenamento dos temas dos resumos.
+    *
+    */
     private String[] temas;
+
+    /**
+    * Atributo auxiliar para ajudar na indexação dos resumos. 
+    * Atributo também responsavel por contar a quantidade de resumos registrados.
+    *
+    */
     private int aux = 0;
 
+    /**
+    * Constrói a lista de String a partir da quantidade definida pelo usuario.
+    *
+    * @param numeroDeResumos Quantidade limite de resumos, no formato “000”
+    */
     public RegistroResumos(int numeroDeResumos){
         this.numeroDeResumos = numeroDeResumos;
         resumos = new String[numeroDeResumos];
         temas = new String[numeroDeResumos];
     }
 
-
+    /**
+    * Metodo que não retorna nenhum valor, responsavel por adicionar a lista resumos,
+    * os respectivos temas e resumos fornecido pelo usuario. O metodo também verifica se
+    * excedeu ou não a quantidade limite dos resumos.
+    *
+    * @param tema tema do resumo, no formato "Exemplo"
+    * @param resumo breve resumo do tema abordado, no formato "Esse pode ser um resumo"
+    */
     public void adiciona(String tema, String resumo){
         if(aux>100){
             aux = 0;
@@ -22,14 +63,34 @@ public class RegistroResumos{
         aux++;
     }
 
+    /**
+    * Retorna a lista com os resumos
+    *
+    *
+    * @return a representação da lista no formato String.
+    */
     public String[] pegaResumos(){
         return resumos;
     }
 
+    /**
+    * Retorna um valor inteiro, associado a quantidade de resumos registrados.
+    *
+    *
+    * @return a representação em inteiro da quantidade de resumos feitos.
+    */
     public int conta(){
         return aux;
     }
 
+    /**
+    * Retorna um concatenação de string. A representação segue o formato 
+    * "Resumos: 
+    * - AUX resumo(s) cadastrado(s)"
+    * "- Tema1 | Tema2 | Tema3"
+    *
+    * @return a representação de uma concatenação no formato String.
+    */
     public String imprimeResumos(){
         String texto = "- " + aux + " resumo(s) cadastrado(s)";
         texto += "\n- ";
@@ -43,13 +104,17 @@ public class RegistroResumos{
             texto += " | ";
 
         }
-        /* 
-        for(int i =0; i<aux; i++){
-            texto += temas[i] + " | ";
-        } */
         return texto;
     }
 
+    /**
+    * Retorna um valor booleano associado a condição apresentada.
+    * Metodo responsavel por verificar se o tema informado contem ou não na lista de temas.
+    *
+    * @param tema tema a ser procurado, no formato "tema1"
+    *
+    * @return a representação em Boolean.
+    */
     public boolean temResumo(String tema){
         boolean verifica = false;
         for(int i=0; i<aux; i++){
