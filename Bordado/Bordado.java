9efc28f50c572b64e0a4818a2d54685ae3c1c754
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-04 22:01:08 -0300

    EM ATUALIZAÇÃO

--- Bordado/Bordado.java
+++ Bordado/Bordado.java
@@ -4,6 +4,8 @@ public class Bordado{
     private int linhas;
     private int colunas;
     private String bordado[][];
+    private int qtdePontos;
+    private double preco;
 
     public Bordado(int id, int linhas, int colunas){
         this.id = id;
@@ -16,9 +18,11 @@ public class Bordado{
         int linha = ponto.getLinhaPonto();
         int coluna = ponto.getColunaPonto();
         this.bordado[linha][coluna] = ponto.getTipoPonto();
+        qtdePontos++;
+        preco = 1 * qtdePontos;
     }
 
-    public String toString(){
+    public String exibiBordado(){
         String formato = "|-----|\n";
         for(int i=0; i<this.linhas; i++){
             for(int j=0; j<bordado[i].length; j++){
@@ -31,6 +35,22 @@ public class Bordado{
             formato += "\n";
         }
         formato += "|-----|\n";
-        return formato ;
+        return formato;
+    }
+
+    public String toString(){
+        return this.id + " - " + this.linhas + " x " + this.colunas + " - " + qtdePontos + " Pontos\n";
+    }
+
+    public void setLinhas(int linha){
+        this.linhas = linha;
+    }
+
+    public void setColunas(int coluna){
+        this.colunas = coluna;
+    }
+
+    public double getPreco(){
+        return preco;
     }
 }
