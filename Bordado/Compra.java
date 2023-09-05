354dec6a0d3167da207bd64e8373ac0b58e9d316
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-04 22:01:19 -0300

    EM ATUALIZAÇÃO

--- Bordado/Compra.java
+++ Bordado/Compra.java
@@ -1,3 +1,41 @@
 public class Compra{
 
+    private Bordado[] bordados;
+    private double valorTotal;
+    private int qtdeComprado;
+    private BEP sistema;
+
+
+    public Compra(){
+        bordados = new Bordado[10];
+        sistema = new BEP();
+    }
+
+
+    public void adicionaCompra(String compra){
+        String[] compras =  compra.split(" ");
+        /* 
+        if(qtdeComprado < 10){
+            for(int i=0; i<compras.length; i++){
+                bordados[qtdeComprado] =  sistema.getBordado(Integer.parseInt(compras[i]));
+                qtdeComprado++;
+
+                valorTotal += bordados[qtdeComprado].getPreco();
+            }
+        }else{
+            qtdeComprado = 0;
+        }*/
+    }
+
+    public String imprimiCompra(){
+        String formatacao = " ";
+        for(int i = 0; i < qtdeComprado; i++){
+            formatacao += "Bordado " + (i+1) + "\n";
+            formatacao += bordados[i].exibiBordado();
+        }
+        formatacao += "\nTotal da Compra: " + valorTotal + "R$";
+        return formatacao;
+    }
+
+
 }
