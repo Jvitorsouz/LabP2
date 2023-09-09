0c561a9908d97634ff9adf250f1ce88890d57b45
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-09 17:59:03 -0300

    Classe atualizada

---
+++ AtividadesExtras/ControleTerrenos/Casas.java
@@ -0,0 +1,10 @@
+public class Casas{
+
+    private int area;
+    private int andares;
+
+    public Casas(int area, int andares){
+        this.area = area;
+        this.andares = andares;
+    }
+}
