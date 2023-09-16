f1852888e709ab14058ddec1d4fcd501e3a860f9
João Vitor da Silva Souza <129335518+Jvitorsouz@users.noreply.github.com>
2023-09-16 20:30:26 -0300

    Add files via upload

---
+++ Agenda/src/agenda/Agenda.java
@@ -0,0 +1,70 @@
+package agenda;
+
+public class Agenda {
+	
+	private static final int TAMANHO_AGENDA = 100;
+	private Contato[] contatos; 
+	private Favoritos favoritos;
+
+	public Agenda() {
+		this.contatos = new Contato[TAMANHO_AGENDA];
+		this.favoritos = new Favoritos();
+	}
+
+	public String cadastraContato(int pos, String nome, String sobrenome, String telefone){
+        if(pos>100 || pos<1){
+            return "\nPOSICAO INVALIDA";
+        }if(nome == ""){
+            return "\nCONTATO INVALIDO";
+        }if(telefone == ""){
+            return "\nCONTATO INVALIDO";
+        }
+
+        Contato contato = new Contato(nome, sobrenome, telefone);
+        for(int i = 0; i<100; i++){
+            if(contatos[i] != null){
+                if(contatos[i].equals(contato)){
+                    return "\nCONTATO JA CADASTRADO";
+                }
+            }
+        }
+
+        contatos[pos-1] = new Contato(nome, sobrenome, telefone);
+        return "\nCADASTRO REALIZADO";
+    }
+
+
+    public String exibirContato(int posicao){
+        if(contatos[posicao-1] == null){
+            return "POSICAO INVALIDA";
+        }
+
+
+        return contatos[posicao-1].exibiContato(); 
+    }
+
+    public String listarContatos(){
+        String formatacao = "\n";
+         for(int i = 0; i<100; i++){
+            if(contatos[i] != null){
+                formatacao += (i+1) + contatos[i].toString();
+            }
+         }
+
+        return formatacao;
+    }
+	
+	public String adicionaFavorito(int posContato, int posFavorito){
+        return favoritos.adiciona(contatos[posContato-1], posFavorito);
+    } 
+
+    public String listarFavoritos(){
+        return favoritos.toString();
+    }
+
+    public void removerFavorito(int posicao2){
+        favoritos.removeFavorito(posicao2);
+
+    }
+
+}
