29983b664531d308139904e95540f8a0e671a8b2
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-17 18:53:12 -0300

    Modificado

--- Agenda/AgendaTests/tests.java
+++ Agenda/AgendaTests/tests.java
@@ -73,5 +73,32 @@ public class tests {
 		
 		assertEquals(resultado, agenda.exibirContato(1));
 	}
+	
+	@Test
+	public void adicionarFavoritoTest() {
+		agenda.cadastraContato(1, "Matheus", "Gaudencio", "(83) 99999-0000");
+		agenda.cadastraContato(2, "Joao", "Vitor", "(83) 99999-1111");
+		agenda.cadastraContato(3, "Alan", "Santos", "(83) 99999-2222");
+		
+		assertEquals(agenda.adicionaFavorito(1, 1), "CONTATO FAVORITADO NA POSIÇÃO 1!");
+		assertEquals(agenda.adicionaFavorito(1, 2), "CONTATO JA ADICIONADO");
+		assertEquals(agenda.adicionaFavorito(2, 5), "CONTATO FAVORITADO NA POSIÇÃO 5!");
+		assertEquals(agenda.adicionaFavorito(3, 10), "CONTATO FAVORITADO NA POSIÇÃO 10!");
+	}
+	
+	@Test
+	public void adicionarFavoritoTest1(){
+		agenda.cadastraContato(1, "Matheus", "Gaudencio", "(83) 99999-0000");
+		assertEquals(agenda.adicionaFavorito(0, 1), "\nPOSICAO INVALIDA!");
+		assertEquals(agenda.adicionaFavorito(1, 0), "\nPOSICAO INVALIDA!");
+		assertEquals(agenda.adicionaFavorito(1, 11), "\nPOSICAO INVALIDA!");
+	}
+	
+	@Test
+	public void listarFavorito(){
+		adicionarFavoritoTest();
+		String resultado = "\n1 - Matheus Gaudencio\n5 - Joao Vitor\n10 - Alan Santos\n";
+		assertEquals(resultado, agenda.listarFavoritos());
+	}
 
 }
