978cbc6fcc4bd0db8b2c937e2e0fcd5b34ae6efe
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-17 19:46:41 -0300

    Documentado

--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -1,16 +1,32 @@
 package agenda;
 
+/**
+ * A classe Agenda representa uma agenda telefônica que pode armazenar até 100 contatos.
+ * Além disso, ela permite a adição e remoção de contatos favoritos.
+ */
 public class Agenda {
 	
 	private static final int TAMANHO_AGENDA = 100;
 	private Contato[] contatos; 
 	private Favoritos favoritos;
 
+    /**
+	 * Construtor da classe Agenda. Inicializa a lista de contatos e a lista de favoritos.
+	 */
 	public Agenda() {
 		this.contatos = new Contato[TAMANHO_AGENDA];
 		this.favoritos = new Favoritos();
 	}
 
+    /**
+	 * Cadastra um novo contato na agenda.
+	 *
+	 * @param pos A posição na agenda onde o contato será cadastrado (de 1 a 100).
+	 * @param nome O nome do contato.
+	 * @param sobrenome O sobrenome do contato.
+	 * @param telefone O telefone do contato.
+	 * @return Uma mensagem indicando o resultado da operação.
+	 */
 	public String cadastraContato(int pos, String nome, String sobrenome, String telefone){
         nome = nome.trim();
         sobrenome = sobrenome.trim();
@@ -37,6 +53,12 @@ public class Agenda {
     }
 
 
+     /**
+	 * Exibe as informações de um contato na posição especificada.
+	 *
+	 * @param posicao A posição do contato a ser exibido (de 1 a 100).
+	 * @return Uma representação em texto do contato ou uma mensagem de erro se a posição for inválida.
+	 */
     public String exibirContato(int posicao){
         if(posicao < 1 || posicao > 100 || contatos[posicao-1] == null){
             return "POSICAO INVALIDA";
@@ -45,6 +67,11 @@ public class Agenda {
         return contatos[posicao-1].exibiContato(); 
     }
 
+    /**
+	 * Lista todos os contatos cadastrados na agenda.
+	 *
+	 * @return Uma string com a lista de contatos cadastrados.
+	 */
     public String listarContatos(){
         String formatacao = "\n";
          for(int i = 0; i<100; i++){
@@ -56,6 +83,13 @@ public class Agenda {
         return formatacao;
     }
 	
+    /**
+	 * Adiciona um contato à lista de favoritos.
+	 *
+	 * @param posContato A posição do contato na agenda a ser adicionado aos favoritos (de 1 a 100).
+	 * @param posFavorito A posição na lista de favoritos onde o contato será adicionado (de 1 a 10).
+	 * @return Uma mensagem indicando o resultado da operação.
+	 */
 	public String adicionaFavorito(int posContato, int posFavorito){
         if(posContato < 1 || posContato > 100){
             return "\nPOSICAO INVALIDA!";
@@ -66,10 +100,20 @@ public class Agenda {
         return favoritos.adiciona(contatos[posContato-1], posFavorito);
     } 
 
+    /**
+	 * Lista todos os contatos favoritos.
+	 *
+	 * @return Uma string com a lista de contatos favoritos.
+	 */
     public String listarFavoritos(){
         return favoritos.toString();
     }
 
+     /**
+	 * Remove um contato da lista de favoritos.
+	 *
+	 * @param posicao2 A posição do contato na lista de favoritos a ser removido (de 1 a 10).
+	 */
     public void removerFavorito(int posicao2){
         favoritos.removeFavorito(posicao2);
 
