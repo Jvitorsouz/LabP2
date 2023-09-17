e61a35e28063f363bf055e8a4888ad421fc8c3bc
Vitor souza <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2023-09-17 19:47:10 -0300

    Documentado

--- Agenda/src/agenda/Favoritos.java
+++ Agenda/src/agenda/Favoritos.java
@@ -1,12 +1,25 @@
 package agenda;
 
+/**
+* A classe Favoritos representa uma lista de contatos favoritos que podem ser adicionados e removidos.
+*/
 public class Favoritos{
     private Contato[] favoritos;
 
+    /**
+    * Construtor da classe Favoritos. Inicializa a lista de favoritos com capacidade para 10 contatos.
+    */
     public Favoritos(){
         favoritos = new Contato[10];
     }
 
+    /**
+    * Adiciona um contato à lista de favoritos em uma posição específica.
+    *
+    * @param contato O contato a ser adicionado aos favoritos.
+    * @param posFavorito A posição na lista de favoritos onde o contato será adicionado (de 1 a 10).
+    * @return Uma mensagem indicando o resultado da operação.
+    */
     public String adiciona(Contato contato, int posFavorito){
         for(int i = 0; i<10; i++){
             if(favoritos[i] != null){
@@ -26,6 +39,11 @@ public class Favoritos{
         return "CONTATO FAVORITADO NA POSIÇÃO " + posFavorito + "!";
     }
 
+    /**
+    * Retorna uma representação em texto da lista de contatos favoritos.
+    *
+    * @return Uma string contendo a lista de contatos favoritos.
+    */
     public String toString(){
         String formatacao = "\n";
         for(int i = 0; i<10; i++){
@@ -36,6 +54,11 @@ public class Favoritos{
         return formatacao;
     }
 
+    /**
+    * Remove um contato da lista de favoritos em uma posição específica.
+    *
+    * @param posicao A posição na lista de favoritos do contato a ser removido (de 1 a 10).
+    */
     public void removeFavorito(int posicao){
         favoritos[posicao-1].setStatus(null);
         favoritos[posicao-1] = null;
