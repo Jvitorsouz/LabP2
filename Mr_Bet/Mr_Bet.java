8c5d09024abe672c4d7ae2ea5cbbde1a0fdc35f5
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-10-03 09:21:02 -0300

    Em att

--- Mr_Bet/Mr_Bet.java
+++ Mr_Bet/Mr_Bet.java
@@ -4,9 +4,11 @@ import java.util.Map;
 public class Mr_Bet{
 
     private HashMap<String, Time> times;
+    private HashMap<String, Campeonato> campeonatos;
 
     public Mr_Bet(){
         times = new HashMap<>();
+        campeonatos = new HashMap<>();
     }
 
     public String incluirTime(String codigo, String nome, String mascote){
@@ -17,6 +19,45 @@ public class Mr_Bet{
         times.put(codigo, new Time(codigo, nome, mascote));
         return "INCLUSÃO REALIZADA!";
     }
+
+    public String recuperaTime(String codigo){
+        if(this.times.containsKey(codigo)){
+            return "[" + codigo + "] " + this.times.get(codigo);
+        }
+
+        return "TIME NÃO EXISTE!";  
+    }
+
+    public String adicionaCampeonato(String campeonato, int qtdeTimes){
+        if(this.campeonatos.containsKey(campeonato)){
+            return "CAMPEONATO JÁ EXISTE!";
+        }
+        campeonatos.put(campeonato, new Campeonato(campeonato, qtdeTimes));
+        return "CAMPEONATO ADICIONADO!";
+    }
+
+    public String addTimeCampeonato(String campeonato, String codigoTime){
+        return "";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void TratamentodeExcecoes(String codigo, String nome, String mascote){
         if(codigo == null && nome == null && mascote == null){
