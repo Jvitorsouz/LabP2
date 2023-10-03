2427f18fb11c32fc13677a4957642f189d86def5
122210993 JOAO VITOR DA SILVA SOUZA <jvitorsouza131203@gmail.com>
2023-10-03 09:20:29 -0300

    Em att

--- Mr_Bet/Campeonato.java
+++ Mr_Bet/Campeonato.java
@@ -1,3 +1,44 @@
 public class Campeonato{
 
+    private String nome;
+    private int qtdeTimes;
+    private Time[] times;
+    private int idx;
+
+    public Campeonato(String nome, int qtdeTimes){
+        this.nome = nome;
+        this.qtdeTimes = qtdeTimes;
+        times = new Time[qtdeTimes];
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Campeonato other = (Campeonato) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    public boolean addTime(Time time){
+        times[idx++] = time;
+        return true;
+    }
+
 }
